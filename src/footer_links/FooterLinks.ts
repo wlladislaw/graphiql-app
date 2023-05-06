1c65c31e0fcee11a0a6decc38ca0be4a2ea5ec90
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-05-06 21:01:11 +0300

    feat: add footer links

---
+++ src/footer_links/FooterLinks.ts
@@ -0,0 +1,26 @@
+
+const Logo = require('../assets/logo.png');
+const GithubLogo = require('../assets/github.png');
+
+export const LINKS_FOOTER = [
+  {
+    name: 'Vlad Stepanov',
+    link: 'https://github.com/wlladislaw/',
+    img: `${GithubLogo}`,
+  },
+  {
+    name: 'Mikhail Matveichuk',
+    link: 'https://github.com/MihailMatveichuk/',
+    img: `${GithubLogo}`,
+  },
+  {
+    name: 'Lary Wildman',
+    link: 'https://github.com/Lary Wildman/',
+    img: `${GithubLogo}`,
+  },
+  {
+    name: 'RSS School',
+    link: 'https://rs.school/js/',
+    img: `${Logo}`,
+  },
+];
