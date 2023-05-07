bf8a3b6bd5cfd021dee1af47003a9f42bfa6d226
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-05-07 10:48:08 +0300

    feat: add button for showing up links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,23 @@
 import './Footer.scss';
+import { useState } from 'react';
 import { LINKS_FOOTER } from './footer_links/FooterLinks';
 import { FooterLogos } from './footer_logos/FooterLogos';
+import Button from 'react-bootstrap/Button';
 
 export const Footer = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const onToggle = () => setToggle(!toggle);
+
   return (
     <footer className="footer">
-      <div className="container">
+      <Button variant="secondary" size="sm" onClick={onToggle}>
+        {toggle ? 'Close links' : 'Open links'}
+      </Button>
+      <div
+        className="container"
+        style={toggle ? { display: 'flex' } : { display: 'none' }}
+      >
         <div className="footer__links">
           <FooterLogos altClass="footer__logos" logos={LINKS_FOOTER} />
         </div>
