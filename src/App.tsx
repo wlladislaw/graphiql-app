f62c47e83efb4470aa4f54622b09ff9d72470d15
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-05-07 20:29:48 +0300

    refactor: delete Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Footer } from './components/footer/Footer';
 import './App.scss';
 import SignIn from './components/Auth/SignIn';
