9d8fc5ae154c89ed4d298127e52f052eba9c2277
Vladislav <wlladisla@gmail.com>
2023-05-07 13:37:39 +0300

    feat: add login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-
+import { Routes, Route, NavLink } from 'react-router-dom';
 import { Footer } from './components/footer/Footer';
 import './App.scss';
+import SignIn from './components/Auth/SignIn';
 
 export function App() {
   return (
     <div className="main-page">
+      <div className="link">
+        <NavLink to="/signIn">
+          <h1>Sign in</h1>
+        </NavLink>
+        <NavLink to="/signUp">
+          <h1>Sign up</h1>
+        </NavLink>
+      </div>
+      <Routes>
+        <Route path="/signIn" element={<SignIn />} />
+      </Routes>
       <Footer />
     </div>
   );
-
 }
---
+++ src/components/Auth/SignIn.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth, logInWithEmailAndPassword } from '../../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import './SignIn.scss';
+function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+    if (user) navigate('/main');
+  }, [user, loading]);
+  return (
+    <div className="signIn">
+      <div className="signIn__container">
+        <input
+          type="text"
+          className="sign__input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="sign__input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button
+          className="sign__btn"
+          onClick={() => logInWithEmailAndPassword(email, password)}
+        >
+          Login
+        </button>
+        <div>
+          Don`t have an account? <Link to="/signUp">Register</Link> now.
+        </div>
+      </div>
+    </div>
+  );
+}
+export default SignIn;
