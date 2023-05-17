f007fef2f2fdfd129da64874fab35cf6a54e44b8
Vladislav <wlladisla@gmail.com>
2023-05-18 00:04:46 +0300

    feat: add base template for schema

--- src/components/Schema/Schema.tsx
+++ src/components/Schema/Schema.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Schema.scss';
 
-function Schema() {
-  return <div>Schema</div>;
-}
+const Schema = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <div>
+      {!isExpanded ? (
+        <button className="schema_btn" onClick={handleButtonClick}>
+          Schema
+        </button>
+      ) : (
+        <div className="schema_container">
+          <input
+            className="schema_input"
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button className="expanded_btn" onClick={handleButtonClick}>
+            Schema
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Schema;
