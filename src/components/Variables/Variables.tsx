41b6b1569cf7b7b93e9485277026a62ea70555a7
Valery <valer4ik-larik@mail.ru>
2023-05-20 02:59:59 +0300

    feat: add basic component and create file for styles

--- src/components/Variables/Variables.tsx
+++ src/components/Variables/Variables.tsx
@@ -1,5 +1,13 @@
+import { useState } from 'react';
+
 const Variables = () => {
-  return <div className="variables_container">variables section</div>;
+  const [showTextArea, setShowTextArea] = useState(true);
+  return (
+    <div>
+      <div className="variables_container">variables section</div>
+      {showTextArea && <textarea className="text-area"></textarea>}
+    </div>
+  );
 };
 
 export default Variables;
