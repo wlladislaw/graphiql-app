e4dc8f5fa28447e682095722d127d829cdabfae2
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-05-20 19:40:58 +0300

    feat: add play button for main page

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Variables from '../Variables/Variables';
 import './Editor.scss';
+import PlayButton from '../../components/PlayButton/PlayButton';
 function Editor() {
   return (
     <section className="editor_container">
       <textarea></textarea>
       <Variables />
+      <PlayButton />
     </section>
   );
 }
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,6 +2,7 @@ import Editor from '../../components/Editor/Editor';
 import QueryInput from '../../components/QueryInput/QueryInput';
 import Response from '../../components/Response/Response';
 import Schema from '../../components/Schema/Schema';
+
 import './Main.scss';
 function Main() {
   return (
