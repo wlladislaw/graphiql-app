c4858249d33ecfa23ee234976f49667b1e262226
Valery <valer4ik-larik@mail.ru>
2023-05-20 03:31:22 +0300

    feat: add logical part

--- src/components/Variables/Variables.tsx
+++ src/components/Variables/Variables.tsx
@@ -2,10 +2,19 @@ import { useState } from 'react';
 import './variables.scss';
 
 const Variables = () => {
-  const [showTextArea, setShowTextArea] = useState(true);
+  const [showTextArea, setShowTextArea] = useState(false);
+  const [textBtn, setTextBtn] = useState('show variables');
+
+  const handler = () => {
+    setShowTextArea(!showTextArea);
+    setTextBtn('hide variables');
+  };
+
   return (
-    <div className="container">
-      <div className="variables_container">variables section</div>
+    <div className="container-var">
+      <button onClick={handler} className="variables_container">
+        {textBtn}
+      </button>
       {showTextArea && <textarea className="text-area"></textarea>}
     </div>
   );
