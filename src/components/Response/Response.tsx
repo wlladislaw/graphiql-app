1bf51e7f513fa87dd55ab12b0ea3a8940a949ec3
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-05-20 19:55:08 +0300

    feat: add response area

--- src/components/Response/Response.tsx
+++ src/components/Response/Response.tsx
@@ -1,6 +1,10 @@
 import './Response.scss';
 function Response() {
-  return <aside className="response_container">Response</aside>;
+  return (
+    <aside className="response_container">
+      Hit the Play Button to get a response here
+    </aside>
+  );
 }
 
 export default Response;
