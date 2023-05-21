4dfba470476b8109c6a328705ada725c3e6805bb
Vladislav <wlladisla@gmail.com>
2023-05-22 00:04:39 +0300

    feat: add handlechange for textarea editor

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,10 +1,18 @@
 import Variables from '../Variables/Variables';
 import './Editor.scss';
 import PlayButton from '../../components/PlayButton/PlayButton';
+import { useState } from 'react';
 function Editor() {
+  const [input, setInput] = useState('');
+  console.log('input: ', input);
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
   return (
     <section className="editor_container">
-      <textarea className="request-area">Try to write your query here</textarea>
+      <textarea className="request-area" onChange={handleChange}>
+        Try to write your query here
+      </textarea>
       <Variables />
       <PlayButton />
     </section>
