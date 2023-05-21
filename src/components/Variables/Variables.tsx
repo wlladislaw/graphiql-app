237c46ac705a526f1b46d762537497627224f419
Valery <valer4ik-larik@mail.ru>
2023-05-22 01:08:54 +0300

    feat: change some block variables

--- src/components/Variables/Variables.tsx
+++ src/components/Variables/Variables.tsx
@@ -2,20 +2,45 @@ import { useState } from 'react';
 import './variables.scss';
 
 const Variables = () => {
-  const [showTextArea, setShowTextArea] = useState(false);
-  const [textBtn, setTextBtn] = useState('show variables');
+  const [showTextAreaQuery, setShowTextAreaQuery] = useState(false);
+  const [showTextAreaHttp, setShowTextAreaHttp] = useState(false);
+  const [classQuery, setClassQuery] = useState('variables_container-1');
+  const [classHttp, setClassHttp] = useState('variables_container-1');
 
-  const handler = () => {
-    setShowTextArea(!showTextArea);
-    setTextBtn('hide variables');
+  const handlerQuery = () => {
+    setShowTextAreaQuery(true);
+    setShowTextAreaHttp(false);
+    setClassQuery('variables_container-2');
+    setClassHttp('variables_container-1');
+  };
+
+  const handlerHttp = () => {
+    setShowTextAreaHttp(true);
+    setShowTextAreaQuery(false);
+    setClassQuery('variables_container-1');
+    setClassHttp('variables_container-2');
+  };
+
+  const handlerHide = () => {
+    setShowTextAreaHttp(false);
+    setShowTextAreaQuery(false);
+    setClassQuery('variables_container-1');
+    setClassHttp('variables_container-1');
   };
 
   return (
     <div className="container-var">
-      <button onClick={handler} className="variables_container">
-        {textBtn}
-      </button>
-      {showTextArea && <textarea className="text-area"></textarea>}
+      <button onClick={handlerHide} className="btn-hide"></button>
+      <div>
+        <button onClick={handlerQuery} className={classQuery}>
+          QUERY VARIABLES
+        </button>
+        <button onClick={handlerHttp} className={classHttp}>
+          HTTP HEADERS
+        </button>
+      </div>
+      {showTextAreaQuery && <textarea className="text-area"></textarea>}
+      {showTextAreaHttp && <textarea className="text-area"></textarea>}
     </div>
   );
 };
