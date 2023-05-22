dbc2af8b038ec54784076a819b3e817e597f8299
Vladislav <wlladisla@gmail.com>
2023-05-23 00:03:23 +0300

    feat: add query api logic with main button

--- src/components/PlayButton/PlayButton.tsx
+++ src/components/PlayButton/PlayButton.tsx
@@ -1,25 +1,25 @@
+import { useAppSelector } from '../../hooks/redux';
+import { useQuery, gql } from '@apollo/client';
 import './PlayButton.scss';
+const mainButton = require('../../assets/mainButton.svg');
+
 function PlayButton() {
+  const { textAreaValue } = useAppSelector((state) => state.editorReducer);
+  console.log('textAreaValue: ', textAreaValue);
+  const handleGetResponse = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { loading, error, data } = useQuery(
+      gql`
+        ${textAreaValue}
+      `
+    );
+    console.log('data:!!!!!!!!!!1 ', data);
+  };
+
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1024 1024"
-      className="button"
-    >
-      <path
-        fill="#rgb(11,20,28)"
-        d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"
-      />
-      <path
-        id="background"
-        fill="rgb(185,191,196)"
-        d="M512 140c-205.4 0-372 166.6-372 372s166.6 372 372 372 372-166.6 372-372-166.6-372-372-372zm164.1 378.2L457.7 677.1a8.02 8.02 0 0 1-12.7-6.5V353a8 8 0 0 1 12.7-6.5l218.4 158.8a7.9 7.9 0 0 1 0 12.9z"
-      />
-      <path
-        fill="#rgb(11,20,28)"
-        d="M676.1 505.3L457.7 346.5A8 8 0 0 0 445 353v317.6a8.02 8.02 0 0 0 12.7 6.5l218.4-158.9a7.9 7.9 0 0 0 0-12.9z"
-      />
-    </svg>
+    <button onClick={handleGetResponse}>
+      <img className="main_button" src={mainButton} alt="main_btn" />
+    </button>
   );
 }
 
