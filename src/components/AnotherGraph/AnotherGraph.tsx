d95056a97cb081849850f4b0221f1824252e620b
Valery <valer4ik-larik@mail.ru>
2023-05-27 20:02:47 +0300

    feat: add some functional

--- src/components/AnotherGraph/AnotherGraph.tsx
+++ src/components/AnotherGraph/AnotherGraph.tsx
@@ -1,3 +1,19 @@
+const altair = require('./assets/altair.png');
+
 export function AnotherGraph() {
-  return <div className="another-graph"></div>;
+  return (
+    <div className="another-graph">
+      <div className="link-container">
+        <a
+          className="link"
+          href="https://altairgraphql.dev/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={altair} alt="" width={'400px'} height={'400px'} />
+          <div className="link-text">Altair GraphQL Client</div>
+        </a>
+      </div>
+    </div>
+  );
 }
