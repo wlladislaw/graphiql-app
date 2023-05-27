f9f1cd564b9c5148ecaaf53bbcd957c61944d839
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-05-27 20:23:56 +0300

    feat: add style for play-button

--- src/components/PlayButton/PlayButton.tsx
+++ src/components/PlayButton/PlayButton.tsx
@@ -1,8 +1,8 @@
 import { useTranslation } from 'react-i18next';
 import { useAppSelector, useAppDispatch } from '../../hooks/redux';
 import { responseSlice } from '../../redux/reducers/responseSlice';
+import MainButton from './MainButton';
 import './PlayButton.scss';
-const mainButton = require('../../assets/mainButton.svg');
 
 function PlayButton() {
   const dispatch = useAppDispatch();
@@ -34,14 +34,7 @@ function PlayButton() {
       .catch(() => dispatch(changeAPIResponse('Error')));
   };
 
-  return (
-    <img
-      className="main_button"
-      src={mainButton}
-      alt="main_btn"
-      onClick={handleGetResponse}
-    />
-  );
+  return <MainButton onClick={handleGetResponse} />;
 }
 
 export default PlayButton;
