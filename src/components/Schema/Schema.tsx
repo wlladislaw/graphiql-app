e9c50c2f99b363f9ea3214cc43dccc3ee19cbdf9
Vladislav <wlladisla@gmail.com>
2023-05-27 21:21:56 +0300

    feat: add base load schema

--- src/components/Schema/Schema.tsx
+++ src/components/Schema/Schema.tsx
@@ -1,11 +1,49 @@
 import { useState } from 'react';
 import './Schema.scss';
 import { useTranslation } from 'react-i18next';
+import { buildClientSchema, getIntrospectionQuery } from 'graphql';
+import { parse } from 'graphql/language';
 
 const Schema = () => {
   const [isExpanded, setIsExpanded] = useState(false);
   const [inputValue, setInputValue] = useState('');
+  const [schema, setSchema] = useState(null);
+
   const { t } = useTranslation();
+
+  const fetchSchema = async () => {
+    const response = await fetch('https://rickandmortyapi.com/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: '{ __schema { types { name } } }' }),
+    });
+
+    const result = await response.json();
+
+    if (result.errors) {
+      console.error('Failed to fetch schema:', result.errors);
+      return;
+    }
+    const introspectionData = result.data.__schema;
+    console.log('introspectionData: ', introspectionData);
+    // const clientSchema = buildClientSchema(introspectionData);
+    // console.log('clientSchema: ', clientSchema);
+
+    //setSchema(clientSchema);
+  };
+
+  const handleParseQuery = (query) => {
+    try {
+      const parsedQuery = parse(query);
+      console.log('parsedQuery: ', parsedQuery);
+
+      // Дополнительная обработка или отображение распарсенного запроса
+    } catch (error) {
+      console.error('Failed to parse query:', error);
+    }
+  };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.currentTarget.value);
@@ -13,6 +51,16 @@ const Schema = () => {
 
   const handleButtonClick = () => {
     setIsExpanded(!isExpanded);
+    fetchSchema();
+    handleParseQuery(`
+    query charactersQuery {
+      characters {
+        results {
+          name
+        }
+      }
+    }
+  `);
   };
 
   return (
